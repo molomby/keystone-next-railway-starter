ed72ed93195e53f2056ec5f1df27227be7677bfe
John Molomby <john@thinkmill.com.au>
2021-05-25 16:15:07 +0930

    Switch to redis based session (though I don't like how KS does this)

--- config.ts
+++ config.ts
@@ -7,13 +7,16 @@ export const PORT = parseInt(process.env.PORT) || 3000;
 export const DATABASE_URL =
   process.env.DATABASE_URL || `postgres://${process.env.USER}@localhost/keystone-next-railway-starter`;
 
+// Redis (session store) URL
+export const REDIS_URL = process.env.REDIS_URL || `redis://localhost`;
+
 // Default to 30 days
 export const SESSION_MAX_AGE = parseInt(process.env.SESSION_MAX_AGE) || 60 * 60 * 24 * 30;
 
 // Default to a secure random string on each app start
 // This will cause all sessions to be revoked unless the env var is suppled
 export const SESSION_SECRET =
-  process.env.SESSION_MAX_AGE ||
+  process.env.SESSION_SECRET ||
   require('crypto')
     .randomBytes(32)
     .toString('base64')
--- keystone.ts
+++ keystone.ts
@@ -1,8 +1,10 @@
+import redis from 'redis';
 import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
+import { redisSessionStore } from '@keystone-next/session-store-redis';
+import { storedSessions, withItemData } from '@keystone-next/keystone/session';
 import { createAuth } from '@keystone-next/auth';
 import { lists } from './schema';
-import { PORT, DATABASE_URL, SESSION_MAX_AGE, SESSION_SECRET } from './config';
+import { PORT, DATABASE_URL, REDIS_URL, SESSION_MAX_AGE, SESSION_SECRET } from './config';
 
 // createAuth configures signin functionality
 const { withAuth } = createAuth({
@@ -26,8 +28,13 @@ export default withAuth(
     server: { port: PORT },
     lists,
     ui: {},
-    session: withItemData(statelessSessions({ maxAge: SESSION_MAX_AGE, secret: SESSION_SECRET }), {
-      Person: 'name isAdmin',
-    }),
+    session: withItemData(
+      storedSessions({
+        maxAge: SESSION_MAX_AGE,
+        secret: SESSION_SECRET,
+        store: redisSessionStore({ client: redis.createClient(REDIS_URL) }),
+      }),
+      { Person: 'name isAdmin' }
+    )
   })
 );
